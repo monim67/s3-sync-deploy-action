bbcc677fe553b4cfd72df6b9d6ad81b01c5939a9
Munim Munna <6266677+monim67@users.noreply.github.com>
2023-03-11 21:02:22 +0600

    Add Construct

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,87 @@
-// import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "path";
 
-export interface S3SyncDeployActionProps {
-  // Define construct properties here
-}
+import { Duration } from "aws-cdk-lib";
+import { ActionBindOptions, ActionCategory, ActionConfig, IStage } from "aws-cdk-lib/aws-codepipeline";
+import { S3DeployActionProps, Action } from "aws-cdk-lib/aws-codepipeline-actions";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Runtime, Code, Function as LambdaFunction } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
-export class S3SyncDeployAction extends Construct {
+export type S3SyncDeployActionProps = S3DeployActionProps;
 
-  constructor(scope: Construct, id: string, props: S3SyncDeployActionProps = {}) {
-    super(scope, id);
+export class S3SyncDeployAction extends Action {
+  private readonly props: S3SyncDeployActionProps;
 
-    // Define construct contents here
+  constructor(props: S3SyncDeployActionProps) {
+    super({
+      ...props,
+      resource: props.bucket,
+      category: ActionCategory.INVOKE,
+      provider: "Lambda",
+      artifactBounds: {
+        minInputs: 1,
+        maxInputs: 1,
+        minOutputs: 0,
+        maxOutputs: 0,
+      },
+      inputs: [props.input],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3SyncDeployActionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.props = props;
+  }
+
+  protected bound(_scope: Construct, _stage: IStage, options: ActionBindOptions): ActionConfig {
+    const syncFn = new LambdaFunction(_scope, "SyncFn", {
+      runtime: Runtime.PYTHON_3_9,
+      memorySize: 1024,
+      timeout: Duration.seconds(60),
+      handler: "index.handler",
+      code: Code.fromAsset(path.join(path.dirname(__dirname), "src")),
+      environment: {
+        BUCKET_NAME: this.props.bucket.bucketName,
+        ...(this.props.objectKey ? { BUCKET_PREFIX: this.props.objectKey } : {}),
+      },
+    });
+
+    if (syncFn.role) {
+      // pipeline needs permissions to read & write to the S3 bucket
+      this.props.bucket.grantReadWrite(syncFn.role);
+
+      if (this.props.accessControl !== undefined) {
+        // we need to modify the ACL settings of objects within the Bucket,
+        // so grant the Action's Role permissions to do that
+        this.props.bucket.grantPutAcl(syncFn.role);
+      }
+    }
+
+    // allow pipeline to list functions
+    options.role.addToPrincipalPolicy(
+      new PolicyStatement({
+        actions: ["lambda:ListFunctions"],
+        resources: ["*"],
+      })
+    );
+
+    // allow pipeline to invoke this lambda functionn
+    syncFn.grantInvoke(options.role);
+
+    // the Action Role also needs to read from the Pipeline's bucket
+    options.bucket.grantRead(options.role);
+
+    // allow lambda to put job results for this pipeline
+    // CodePipeline requires this to be granted to '*'
+    // (the Pipeline ARN will not be enough)
+    syncFn.addToRolePolicy(
+      new PolicyStatement({
+        resources: ["*"],
+        actions: ["codepipeline:PutJobSuccessResult", "codepipeline:PutJobFailureResult"],
+      })
+    );
+
+    return {
+      configuration: {
+        FunctionName: syncFn.functionName,
+      },
+    };
   }
 }
---
+++ src/index.py
@@ -0,0 +1,65 @@
+import mimetypes
+import os
+import shutil
+import tempfile
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+import boto3
+
+if TYPE_CHECKING:
+    from aws_lambda_typing.events import CodePipelineEvent
+
+
+def handler(event: "CodePipelineEvent", context: Any) -> None:
+    codepipeline = boto3.client("codepipeline")
+    return sync_artifact(event, codepipeline)
+
+
+def sync_artifact(event: "CodePipelineEvent", codepipeline: Any) -> None:
+    def fail(message: str):
+        codepipeline.put_job_failure_result(
+            jobId=job["id"], failureDetails={"type": "JobFailed", "message": message}
+        )
+
+    job = event["CodePipeline.job"]
+    bucket_name = os.getenv("BUCKET_NAME")
+    bucket_prefix = (os.getenv("BUCKET_PREFIX", "") + "/").lstrip("/")
+    if not bucket_name:
+        return fail("Missing env BUCKET_NAME")
+    if len(job["data"]["inputArtifacts"]) != 1:
+        return fail("Artifact count should be 1")
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdirpath = Path(tmpdirname)
+        with tempfile.NamedTemporaryFile() as tmp_file:
+            s3_location = job["data"]["inputArtifacts"][0]["location"]["s3Location"]
+            artifactCredentials = job["data"]["artifactCredentials"]
+            boto3.client(
+                "s3",
+                aws_access_key_id=artifactCredentials["accessKeyId"],
+                aws_secret_access_key=artifactCredentials["secretAccessKey"],
+                aws_session_token=artifactCredentials["sessionToken"],
+            ).download_fileobj(
+                s3_location["bucketName"], s3_location["objectKey"], tmp_file
+            )
+            tmp_file.seek(0)
+            shutil.unpack_archive(tmp_file.name, tmpdirname, "zip")
+        s3 = boto3.client("s3")
+        s3_paginator = s3.get_paginator("list_objects_v2")
+        for page in s3_paginator.paginate(Bucket=bucket_name, Prefix=bucket_prefix):
+            for content in page.get("Contents", ()):
+                file_name = content["Key"][len(bucket_prefix) :]
+                if not (tmpdirpath / file_name).exists():
+                    s3.delete_object(Bucket=bucket_name, Key=content["Key"])
+        for file_name in tmpdirpath.glob("**/*"):
+            if not file_name.is_dir():
+                content_type, _ = mimetypes.guess_type(file_name)
+                s3.upload_file(
+                    Filename=str(file_name),
+                    Bucket=bucket_name,
+                    Key=f"{bucket_prefix}{file_name.relative_to(tmpdirpath)}",
+                    ExtraArgs={"ContentType": content_type},
+                )
+
+    codepipeline.put_job_success_result(jobId=job["id"])
